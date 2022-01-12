6a0a537c088e7c11ab86db42965b24704d860526
Aman Sagar <amansagar2020@yahoo.com>
2022-01-13 01:50:11 +0530

    changes in seed.js

--- seeds/index.js
+++ seeds/index.js
@@ -1,9 +1,14 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
+
 const mongoose = require('mongoose');
 const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
@@ -26,7 +31,7 @@ const seedDB = async(req, res) => {
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
 
-            author: '61df12d29f2ece0365d696ca',
+            author: '61df34423e589a04c242b7ef',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
